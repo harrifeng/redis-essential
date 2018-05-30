f96c6f3457c0e3daecd482ff883b15e306ec5735
Haoran Feng <harrifeng@gmail.com>
2018-05-30 07:44:53 +0000

    not ok

--- ch2/02.js
+++ ch2/02.js
@@ -5,42 +5,13 @@ function LeaderBoard(key) {
   this.key = key;
 }
 
+
 LeaderBoard.prototype.addUser = function(username, score) {
   client.zadd([this.key, score, username], function(err, replies) {
     console.log("User", username, "added to the leaderboard!")
   });
 };
 
-LeaderBoard.prototype.removeUser = function(username) {
-  client.zrem(this.key, username, function(err, replies) {
-    console.log("User", username, "remove successfully!");
-  });
-};
-
-LeaderBoard.prototype.getUserScoreAndRank = function(username) {
-  var leaderboardKey = this.key;
-  client.zscore(leaderboardKey, username, function(err, zscoreReply) {
-    clieint.zrevrank(leaderboardKey, username, function(
-      err, zreverankReply) {
-      console.log("\nDetails of " + username + ":");
-      console.log("Score: ", zscoreReply + ", Rank: #" + (zreverankReply + 1));
-    });
-  });
-};
-
-LeaderBoard.prototype.showTopUsers = function(quantity) {
-  client.zrevrange([this.key, 0, quantity - 1, "WITHSCORES"],
-                   function(err,
-                            reply) {
-                     console.log("\nTop", quantity, "users:");
-                     for (var i = 0, rank = 1; i < reply.length; i += 2, rank++) {
-                       console.log("#" + rank, "User: " + reply[i] + ", score:", reply[i+1]);
-                     }
-                   });
-}
 
 var leaderBoard = new LeaderBoard("game-score");
-LeaderBoard.addUser("Arthur", 70);
-LeaderBoard.addUser("KC", 20);
-LeaderBoard.addUser("Maxwell", 10);
 LeaderBoard.addUser("Patrik", 70);
